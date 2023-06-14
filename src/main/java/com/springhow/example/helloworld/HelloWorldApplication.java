ffe9c80ac37939f0cad5cf9a133be4b871f60a66
sandeepkumar1701 <118842173+sandeepkumar1701@users.noreply.github.com>
2023-06-14 20:15:10 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "test deploy 4";
+        return "please visit seabuckthorn";
     }
 
 }
